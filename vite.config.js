9f196cec3c13b861893fadfc4425243922431110
muhammeddyayla <muhammeddyayla@gmail.com>
2025-11-22 08:08:35 +0300

    Fix GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Dizimey/",
+  base: '/Dizimey/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
